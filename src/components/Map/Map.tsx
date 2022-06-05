a53bc6beba8f20dd6d931bd8cbf4df0fbf50659a
elena_kau <lelickau@yandex.ru>
2022-06-05 13:24:56 +0200

    Add Map component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,28 @@
 import React, { FC } from 'react'
+import GoogleMapReact from 'google-map-react'
+import { useMediaQuery } from '@material-ui/core'
+
+import useStyles from './styles'
 
 const Map: FC = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width: 600px)')
+    const coordinates = {lat: 0, lng: 0}
+
     return (
-        <div>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: 'AIzaSyBGYQxLBpB9-VOrX20WNDv2UXB4MTWDHGw'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                // options={''}
+                // onChange={''}
+                // onChildClick={''}
+            >
+
+            </GoogleMapReact>
 
         </div>
     )
