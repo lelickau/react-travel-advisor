13c5332af8af7826004b3dce5fe383a675288b82
elena_kau <lelickau@yandex.ru>
2022-06-06 18:57:11 +0200

    Add PlaceDetails component

--- src/components/PlaceDetails/PlaceDetails.tsx
+++ src/components/PlaceDetails/PlaceDetails.tsx
@@ -1,10 +1,93 @@
+import { CardMedia, Card, CardContent, Typography, Box, Chip, CardActions, Button } from '@material-ui/core'
 import React, { FC } from 'react'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import PhoneIcon from '@material-ui/icons/Phone'
+import Rating from '@material-ui/lab/Rating'
 
-const PlaceDetails: FC = () => {
+import useStyles from './styles'
+
+interface IPlace {
+    name: string;
+    photo: any;
+    price_level: string;
+    ranking: string;
+    awards: any;
+    cuisine: any;
+    address: string;
+    phone: string;
+    web_url: string;
+    website: string;
+    rating: string;
+    num_reviews: number;
+}
+interface PlaceDetailsProps {
+    place: IPlace;
+}
+
+const PlaceDetails: FC<PlaceDetailsProps> = ({place}) => {
+    const classes = useStyles()
     return (
-        <div>
-
-        </div>
+        <Card elevation={6}>
+            <CardMedia
+                style={{ height: 350 }}
+                image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between" my={2}>
+                    <Rating name="read-only" value={Number(place.rating)} readOnly />
+                    <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography component="legend">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                        {place.price_level}
+                    </Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography component="legend">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                        {place.ranking}
+                    </Typography>
+                </Box>
+                {place?.awards?.map((award: any) => (
+                    <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
+                        <img src={award.images.small} />
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map(({ name }: {name: string}) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip} />
+                ))}
+                {place.address && (
+                    <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon />{place.address}
+                    </Typography>
+                )}
+                {place.phone && (
+                    <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+                        <PhoneIcon /> {place.phone}
+                    </Typography>
+                )}
+            </CardContent>
+            <CardActions>
+                <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => window.open(place.web_url, '_blank')}
+                >
+                Trip Advisor
+                </Button>
+                <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => window.open(place.website, '_blank')}
+                >
+                Website
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
 
